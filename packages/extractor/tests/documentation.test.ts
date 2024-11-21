764aa40eddde0aa22a12102ca70d40ded035d8e1
Mateusz Kadlubowski <xeho91@pm.me>
2024-11-21 21:23:29 +0800

    split tests

---
+++ packages/extractor/tests/documentation.test.ts
@@ -0,0 +1,138 @@
+import { describe, it } from "vitest";
+
+import { DocumentationExtractor } from "../src/documentation.js";
+import { Parser } from "../src/parser.js";
+import { create_path_to_example_component } from "./util.js";
+
+describe("DocumentationExtractor", () => {
+	describe("description", () => {
+		it("returns empty string when no description is found", ({ expect }) => {
+			const filepath = create_path_to_example_component(
+				"extractor",
+				"documentation",
+				"description",
+				"empty.svelte",
+			);
+			const parsed = new Parser(filepath);
+			// biome-ignore lint/style/noNonNullAssertion: No need to add type guard
+			const extracted = new DocumentationExtractor(parsed.documentation_comment!);
+			expect(extracted.description).toBeDefined();
+			expect(extracted.description).toBe("");
+		});
+
+		it("extracts description correctly after `@component` tag", ({ expect }) => {
+			const filepath = create_path_to_example_component(
+				"extractor",
+				"documentation",
+				"description",
+				"with-tags.svelte",
+			);
+			const parsed = new Parser(filepath);
+			// biome-ignore lint/style/noNonNullAssertion: No need to add type guard
+			const extracted = new DocumentationExtractor(parsed.documentation_comment!);
+			expect(extracted.description).toBeDefined();
+			expect(extracted.description).toMatchInlineSnapshot(`"Native button component description"`);
+		});
+
+		it("removes leading and trailing whitespaces", ({ expect }) => {
+			const filepath = create_path_to_example_component(
+				"extractor",
+				"documentation",
+				"description",
+				"whitespaced.svelte",
+			);
+			const parsed = new Parser(filepath);
+			// biome-ignore lint/style/noNonNullAssertion: No need to add type guard
+			const extracted = new DocumentationExtractor(parsed.documentation_comment!);
+			expect(extracted.description).toBeDefined();
+			expect(extracted.description).toMatchInlineSnapshot(
+				`"Leading an trailing whitespaces from description are removed."`,
+			);
+		});
+
+		it("extracts with multiple lines correctly", ({ expect }) => {
+			const filepath = create_path_to_example_component(
+				"extractor",
+				"documentation",
+				"description",
+				"multiple-lines.svelte",
+			);
+			const parsed = new Parser(filepath);
+			// biome-ignore lint/style/noNonNullAssertion: No need to add type guard
+			const extracted = new DocumentationExtractor(parsed.documentation_comment!);
+			expect(extracted.description).toBeDefined();
+			expect(extracted.description).toMatchInlineSnapshot(`
+				"This is first paragraph.
+
+
+				This is second paragraph.
+
+
+
+				This is third paragraph.
+
+
+				---
+
+				This is footnote;"
+			`);
+		});
+	});
+
+	describe("tags", () => {
+		it("returns empty array when no other tags other than `@component` are found", ({ expect }) => {
+			const filepath = create_path_to_example_component("extractor", "documentation", "tags", "no-tags.svelte");
+			const parsed = new Parser(filepath);
+			// biome-ignore lint/style/noNonNullAssertion: No need to add type guard
+			const extracted = new DocumentationExtractor(parsed.documentation_comment!);
+			expect(extracted.tags).toHaveLength(0);
+		});
+
+		it("extracts an array of tags correctly", ({ expect }) => {
+			const filepath = create_path_to_example_component("extractor", "documentation", "tags", "example.svelte");
+			const parsed = new Parser(filepath);
+			// biome-ignore lint/style/noNonNullAssertion: No need to add type guard
+			const extracted = new DocumentationExtractor(parsed.documentation_comment!);
+			expect(extracted.tags).toHaveLength(2);
+			expect(extracted.tags).toContainEqual({
+				name: "@category",
+				description: "Atom",
+			});
+			expect(extracted.tags).toContainEqual({
+				name: "@subcategory",
+				description: "Semantic",
+			});
+		});
+
+		it("complicated tag with multiple lines is parsed correctly", ({ expect }) => {
+			const filepath = create_path_to_example_component("extractor", "documentation", "tags", "complex.svelte");
+			const parsed = new Parser(filepath);
+			// biome-ignore lint/style/noNonNullAssertion: No need to add type guard
+			const extracted = new DocumentationExtractor(parsed.documentation_comment!);
+			expect(extracted.tags).toHaveLength(1);
+			const customTag = extracted.tags.find((tag) => tag.name === "@custom");
+			expect(customTag?.description).toMatchInlineSnapshot(`
+				"Very complex tag with {@link https://example.com}
+				and multi-line {@link https://example.com}.
+
+				It also should ignore markdownlint syntax unless some of lines starts with \`@\`.
+
+				## Some heading
+
+				Some text.
+
+				---
+
+				## Another heading
+
+				Some [text].
+
+				| Tables | Are | Cool |
+				| ------ | --- | ---- |
+				|        |     |      |
+
+				[text]: https://example.com"
+			`);
+		});
+	});
+});
---
+++ packages/extractor/tests/prop.test.ts
@@ -0,0 +1,20 @@
+import { describe, it } from "vitest";
+
+import { Extractor } from "../src/mod.js";
+import { PropExtractor } from "../src/prop.js";
+import { create_path_to_example_component } from "./util.js";
+
+describe("PropExractor", () => {
+	it("creates properties required for further transformation correctly", ({ expect }) => {
+		const filepath = create_path_to_example_component("extractor", "props", "rune.svelte");
+		const extracted = new Extractor(filepath);
+		for (const [name, prop] of extracted.props) {
+			expect(name).toBe("disabled");
+			expect(prop).toBeInstanceOf(PropExtractor);
+			expect(prop).toHaveProperty("symbol");
+			expect(prop).toHaveProperty("type");
+			expect(prop).toHaveProperty("declaration");
+			expect(prop).toHaveProperty("source");
+		}
+	});
+});
