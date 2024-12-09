fa42efce6d7b4a8cd13c8f396d91e6f60abc5b8c
Mateusz Kadlubowski <xeho91@pm.me>
2024-12-09 11:04:22 +0800

    fix: Use project root path instead of `process.cwd()`

--- tests/snapshot-serializer.ts
+++ tests/snapshot-serializer.ts
@@ -1,8 +1,15 @@
+import path from "node:path";
 import process from "node:process";
+import url from "node:url";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import type { SnapshotSerializer } from "vitest";
 
+const ROOT_PATH = path.join(__dirname, "..");
+
 export default {
-	test: (value) => typeof value === "string" && value.includes(process.cwd()),
-	print: (value) => (typeof value === "string" ? value.replace(process.cwd(), "<process-cwd>") : ""),
+	test: (value) => typeof value === "string" && value.includes(ROOT_PATH),
+	print: (value) => (typeof value === "string" ? value.replace(ROOT_PATH, "<process-cwd>") : ""),
 } satisfies SnapshotSerializer;
