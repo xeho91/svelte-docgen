93a7251bda71b4fc6f4114d2e894e9dbbe027a2f
ciscorn <ciscorn@users.noreply.github.com>
2024-12-12 16:40:21 +0000

    Deploy preview for PR 22 🛫

---
+++ docs/pr-preview/pr-22/media/description.svelte
@@ -0,0 +1,17 @@
+<!--
+	@component This is a component description.
+
+	It can be multi-line too.
+
+	## Markdown support
+
+	You can also put some **markdown** syntax inside.
+	_As long as you parse it by yourself!_
+
+	| Package                                            | Description                                              |
+	| -------------------------------------------------- | -------------------------------------------------------- |
+	| [`svelte-docgen`](./packages/svelte-docgen)        | Core package                                             |
+	| [`@svelte-docgen/cli`](./packages/cli)             | Standalone CLI                                           |
+	| [`@svelte-docgen/extractor`](./packages/extractor) | Extracting types from `*.svelte` files to write your own |
+	| [`@svelte-docgen/server`](./packages/server)       | Run a server which allows fetching docs on demand        |
+-->
