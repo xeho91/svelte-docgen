9279ecb48bc1a5dc11986edf8e65bc7ea2c7c891
ciscorn <ciscorn@users.noreply.github.com>
2024-12-12 16:08:37 +0000

    Deploy preview for PR 21 🛫

---
+++ docs/pr-preview/pr-21/media/tags.svelte
@@ -0,0 +1,28 @@
+<!--
+	@component This is a component description.
+
+	@category Atom
+	@subcategory Native
+	@author xeho91
+
+	@example Basic usage
+
+	Use it if you want to use **default** design.
+
+	```svelte
+	<Button onclick={() => alert("Awesome!")}>Click me</Button>
+	```
+
+	@example Advanced usage
+
+	```svelte
+	<Button
+		onclick={() => alert("Awesome!")}
+		variant="outlined"
+		color="secondary"
+		size="small"
+	>
+		Click me
+	</Button
+	```
+-->
