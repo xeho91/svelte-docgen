dc8b81081608bad66a896a519971d2ae235ed1c7
Mateusz Kadlubowski <xeho91@pm.me>
2024-12-14 16:50:14 +0800

    document post-build script

--- packages/vite-plugin-svelte-docgen/scripts/post-build.ts
+++ packages/vite-plugin-svelte-docgen/scripts/post-build.ts
@@ -5,6 +5,12 @@ import url from "node:url";
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+/**
+ * We can't re-export types using JSDoc, sadly.
+ * Hence we append the module declaration with import part query parameter to provide a better TypeScript experience for users.
+ *
+ * NOTE: Is included in `build:pkg` script
+ */
 function main(): void {
 	const types_filepath = url.pathToFileURL(path.join(__dirname, "..", "types", "mod.d.ts"));
 	const content = `
