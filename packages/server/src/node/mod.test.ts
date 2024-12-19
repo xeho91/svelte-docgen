ed980178971962cf4d1af9c7463df5798ca41826
Mateusz Kadlubowski <xeho91@pm.me>
2024-12-19 14:02:28 +0800

    improve tests

--- packages/server/src/node/mod.test.ts
+++ packages/server/src/node/mod.test.ts
@@ -32,7 +32,152 @@ describe("NodeServer", () => {
 			const data = await server.request({
 				filepath: path.join(__dirname, "..", "..", "examples", "test.svelte"),
 			});
-			expect(data).toMatchInlineSnapshot(`{}`);
+			expect(data).toMatchInlineSnapshot(`
+				{
+				  "exports": Map {},
+				  "isLegacy": false,
+				  "props": Map {
+				    "custom" => {
+				      "isBindable": false,
+				      "isExtended": true,
+				      "isOptional": false,
+				      "sources": Set {
+				        "/packages/server/examples/test.svelte",
+				      },
+				      "tags": [],
+				      "type": {
+				        "kind": "string",
+				      },
+				    },
+				    "children" => {
+				      "isBindable": false,
+				      "isExtended": true,
+				      "isOptional": true,
+				      "sources": Set {
+				        "/packages/server/examples/test.svelte",
+				      },
+				      "tags": [],
+				      "type": {
+				        "kind": "union",
+				        "nonNullable": {
+				          "alias": "Snippet",
+				          "calls": [
+				            {
+				              "parameters": [
+				                {
+				                  "isOptional": false,
+				                  "name": "args",
+				                  "type": {
+				                    "elements": [],
+				                    "isReadonly": false,
+				                    "kind": "tuple",
+				                  },
+				                },
+				              ],
+				              "returns": {
+				                "kind": "intersection",
+				                "types": [
+				                  {
+				                    "kind": "interface",
+				                    "members": Map {
+				                      "{@render ...} must be called with a Snippet" => {
+				                        "isOptional": false,
+				                        "isReadonly": false,
+				                        "type": {
+				                          "kind": "literal",
+				                          "subkind": "string",
+				                          "value": "import type { Snippet } from 'svelte'",
+				                        },
+				                      },
+				                    },
+				                  },
+				                  {
+				                    "kind": "literal",
+				                    "subkind": "symbol",
+				                  },
+				                ],
+				              },
+				            },
+				          ],
+				          "kind": "function",
+				          "sources": Set {
+				            /node_modules/.pnpm/svelte@<semver>/node_modules/svelte/types/index.d.ts,
+				          },
+				        },
+				        "types": [
+				          {
+				            "kind": "undefined",
+				          },
+				          {
+				            "alias": "Snippet",
+				            "calls": [
+				              {
+				                "parameters": [
+				                  {
+				                    "isOptional": false,
+				                    "name": "args",
+				                    "type": {
+				                      "elements": [],
+				                      "isReadonly": false,
+				                      "kind": "tuple",
+				                    },
+				                  },
+				                ],
+				                "returns": {
+				                  "kind": "intersection",
+				                  "types": [
+				                    {
+				                      "kind": "interface",
+				                      "members": Map {
+				                        "{@render ...} must be called with a Snippet" => {
+				                          "isOptional": false,
+				                          "isReadonly": false,
+				                          "type": {
+				                            "kind": "literal",
+				                            "subkind": "string",
+				                            "value": "import type { Snippet } from 'svelte'",
+				                          },
+				                        },
+				                      },
+				                    },
+				                    {
+				                      "kind": "literal",
+				                      "subkind": "symbol",
+				                    },
+				                  ],
+				                },
+				              },
+				            ],
+				            "kind": "function",
+				            "sources": Set {
+				              /node_modules/.pnpm/svelte@<semver>/node_modules/svelte/types/index.d.ts,
+				            },
+				          },
+				        ],
+				      },
+				    },
+				  },
+				}
+			`);
+			server.shutdown();
+		});
+
+		it("it allows picking selected keys", async ({ expect }) => {
+			const server = createServer();
+			server.start();
+			const data = await server.request({
+				filepath: path.join(__dirname, "..", "..", "examples", "test.svelte"),
+				keys: ["isLegacy"],
+			});
+			expect(data).toMatchInlineSnapshot(`
+				{
+				  "isLegacy": false,
+				}
+			`);
+			expect(data.isLegacy).toBeDefined();
+			// @ts-expect-error Testing
+			expect(data.props).not.toBeDefined();
+			expect(Object.keys(data).length).toBe(1);
 			server.shutdown();
 		});
 	});
